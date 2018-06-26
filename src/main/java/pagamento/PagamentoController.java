700cff88a571bc8d50cd7639142086c2aa4e24f0
bruno1022 <40252090+bruno1022@users.noreply.github.com>
2018-06-25 22:39:26 -0300

    update pagamento

--- src/main/java/pagamento/PagamentoController.java
+++ src/main/java/pagamento/PagamentoController.java
@@ -4,20 +4,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PagamentoController {
 
-    
-    @RequestMapping("/v1/pagamento/info")
-    public String hello() {
-    	
-        return "{\"mensagem\":\"Bem Vindo a API de Pagamentos!\"}";
-    }
-    
-	@PostMapping("/v1/pagamento/")    
+	@PostMapping("/v1/pagamento")    
     public ResponseEntity<?> efetuaPagamento(@RequestBody Pagamento pagamento) {
 		
 		if(pagamento!=null) {
@@ -25,9 +17,6 @@ public class PagamentoController {
 		} 
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-		
-		
-				
     }
 
 }
